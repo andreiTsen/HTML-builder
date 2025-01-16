3b7d5e670ab4ffb5491a0f4db72a374d4e93873d
Andrei Tseniuta <gomelmf@gmail.com>
2025-01-16 17:30:59 +0100

    feat: copy files

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,28 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function copyDir() {
+    const sourceDir = path.join(__dirname, 'files');
+    const targetDir = path.join(__dirname, 'files-copy');
+
+    try {
+        await fs.rm(targetDir, { recursive: true, force: true });
+        
+        await fs.mkdir(targetDir, { recursive: true });
+        
+        const files = await fs.readdir(sourceDir);
+        
+        const copyPromises = files.map(file => {
+            const sourcePath = path.join(sourceDir, file);
+            const targetPath = path.join(targetDir, file);
+            return fs.copyFile(sourcePath, targetPath);
+        });
+        
+        await Promise.all(copyPromises);
+        
+    } catch (error) {
+        console.error('Ошибка при копировании директории:', error);
+    }
+}
+
+copyDir();
