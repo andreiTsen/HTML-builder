3e4cb412d449a73bece7fe8993f285f18316ae98
Andrei Tseniuta <gomelmf@gmail.com>
2025-01-23 16:27:46 +0100

    finnaly fix

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -7,24 +7,27 @@ const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
 
 const rl = readline.createInterface({
     input: process.stdin,
-    output: process.stdout
+    output: process.stdout,
+    prompt: ''
 });
+
+function sayGoodbye() {
+    console.log('\nДо свидания! Хорошего дня!');
+    rl.close();
+}
 
 console.log('Добро пожаловать! Введите текст (введите "exit" для выхода):');
 
 rl.on('line', (input) => {
     if (input.trim().toLowerCase() === 'exit') {
-        console.log('До свидания! Хорошего дня!');
-        rl.close();
+        sayGoodbye();
         return;
     }
     writeStream.write(input + '\n');
 });
 
-process.on('SIGINT', () => {
-    console.log('\nДо свидания! Хорошего дня!');
-    rl.close();
-});
+rl.on('SIGINT', sayGoodbye);
+process.on('SIGINT', sayGoodbye);
 
 rl.on('close', () => {
     writeStream.end();
